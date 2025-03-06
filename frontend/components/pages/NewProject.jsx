58d729cd83bb23520c2076a1fa7271fd801f28cc
Anderson Bernardo <anderson.bernardo508@gmail.com>
2025-03-06 01:26:17 -0300

    Criação/estilização da pagina new project e conexão com api

--- frontend/components/pages/NewProject.jsx
+++ frontend/components/pages/NewProject.jsx
@@ -1,8 +1,36 @@
+import styles from "./NewProject.module.css";
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+import { useState, useEffect } from "react";
+
 function NewProject() {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/categories", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => setCategories(data))
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
-        <>
-            <h1>New Project</h1>
-        </>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+
+            <form>
+                <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
+                <Input type="number" text="Orçamento do projeto" name="name" placeholder="Insira o orçamento total" />
+                <Select name="category_id" text="Selecione a categoria" options={categories} />
+                <SubmitButton text="Criar Projeto" />
+            </form>
+        </div>
     );
 }
 
